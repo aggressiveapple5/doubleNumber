200127025d790681e9f5e14982a13605c76b6e73
Matthew Keller <mkeller2019@pingry.org>
2016-03-04 10:48:26 -0500

    Program is fully functional and says to try again if float is not entered

---
+++ doubleNumber.py
@@ -0,0 +1,19 @@
+error = True
+while error == True:
+	number = raw_input("What number should I double? ")
+
+	try: 
+		number = float(number)
+		#Try is only for things that might not work
+	except ValueError:
+		print("Sorry that's not a number. ")
+		print("Try again.")
+		error = True
+	
+	
+	else:
+		print("Double that is {}.".format(number * 2))
+		error = False
+	
+	
+	
